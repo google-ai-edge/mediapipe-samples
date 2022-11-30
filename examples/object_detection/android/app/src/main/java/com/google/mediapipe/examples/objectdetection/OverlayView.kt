84c8188c2f5811204d52ba9ffe65b8f2e6a68e7b
Duy Mai M <duy.mai@supremetech.vn>
2022-11-30 15:31:56 +0700

    sync the settings between camera and gallery screen

--- examples/object_detection/android/app/src/main/java/com/google/mediapipe/examples/objectdetection/OverlayView.kt
+++ examples/object_detection/android/app/src/main/java/com/google/mediapipe/examples/objectdetection/OverlayView.kt
@@ -24,100 +24,114 @@ import androidx.core.content.ContextCompat
 import com.google.mediapipe.tasks.vision.objectdetector.ObjectDetectionResult
 import kotlin.math.min
 
-class OverlayView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+class OverlayView(context: Context?, attrs: AttributeSet?) :
+    View(context, attrs) {
 
-  private var results: ObjectDetectionResult? = null
-  private var boxPaint = Paint()
-  private var textBackgroundPaint = Paint()
-  private var textPaint = Paint()
+    private var results: ObjectDetectionResult? = null
+    private var boxPaint = Paint()
+    private var textBackgroundPaint = Paint()
+    private var textPaint = Paint()
 
-  private var scaleFactor: Float = 1f
+    private var scaleFactor: Float = 1f
 
-  private var bounds = Rect()
+    private var bounds = Rect()
 
-  init {
-    initPaints()
-  }
-
-  fun clear() {
-    results = null
-    textPaint.reset()
-    textBackgroundPaint.reset()
-    boxPaint.reset()
-    invalidate()
-    initPaints()
-  }
-
-  private fun initPaints() {
-    textBackgroundPaint.color = Color.BLACK
-    textBackgroundPaint.style = Paint.Style.FILL
-    textBackgroundPaint.textSize = 50f
-
-    textPaint.color = Color.WHITE
-    textPaint.style = Paint.Style.FILL
-    textPaint.textSize = 50f
-
-    boxPaint.color = ContextCompat.getColor(context!!, R.color.mp_primary)
-    boxPaint.strokeWidth = 8F
-    boxPaint.style = Paint.Style.STROKE
-  }
-
-  override fun draw(canvas: Canvas) {
-    super.draw(canvas)
-
-    results?.let {
-      for (detection in it.detections()) {
-        val boundingBox = detection.boundingBox()
-
-        val top = boundingBox.top * scaleFactor
-        val bottom = boundingBox.bottom * scaleFactor
-        val left = boundingBox.left * scaleFactor
-        val right = boundingBox.right * scaleFactor
-
-        // Draw bounding box around detected objects
-        val drawableRect = RectF(left, top, right, bottom)
-        canvas.drawRect(drawableRect, boxPaint)
-
-        // Create text to display alongside detected objects
-        val drawableText =
-          detection.categories()[0].categoryName() +
-            " " +
-            String.format("%.2f", detection.categories()[0].score())
-
-        // Draw rect behind display text
-        textBackgroundPaint.getTextBounds(drawableText, 0, drawableText.length, bounds)
-        val textWidth = bounds.width()
-        val textHeight = bounds.height()
-        canvas.drawRect(
-          left,
-          top,
-          left + textWidth + Companion.BOUNDING_RECT_TEXT_PADDING,
-          top + textHeight + Companion.BOUNDING_RECT_TEXT_PADDING,
-          textBackgroundPaint
-        )
-
-        // Draw text for detected object
-        canvas.drawText(drawableText, left, top + bounds.height(), textPaint)
-      }
+    init {
+        initPaints()
     }
-  }
 
-  fun setResults(
-    detectionResults: ObjectDetectionResult,
-    imageHeight: Int,
-    imageWidth: Int,
-  ) {
-    results = detectionResults
+    fun clear() {
+        results = null
+        textPaint.reset()
+        textBackgroundPaint.reset()
+        boxPaint.reset()
+        invalidate()
+        initPaints()
+    }
 
-    // Images, videos and camera live streams are displayed in FIT_START mode. So we need to scale
-    // up the bounding box to match with the size that the images/videos/live streams being
-    // displayed.
-    scaleFactor = min(width * 1f / imageWidth, height * 1f / imageHeight)
+    private fun initPaints() {
+        textBackgroundPaint.color = Color.BLACK
+        textBackgroundPaint.style = Paint.Style.FILL
+        textBackgroundPaint.textSize = 50f
 
-    invalidate()
-  }
+        textPaint.color = Color.WHITE
+        textPaint.style = Paint.Style.FILL
+        textPaint.textSize = 50f
 
-  companion object {
-    private const val BOUNDING_RECT_TEXT_PADDING = 8
-  }
+        boxPaint.color = ContextCompat.getColor(context!!, R.color.mp_primary)
+        boxPaint.strokeWidth = 8F
+        boxPaint.style = Paint.Style.STROKE
+    }
+
+    override fun draw(canvas: Canvas) {
+        super.draw(canvas)
+
+        results?.let {
+            for (detection in it.detections()) {
+                val boundingBox = detection.boundingBox()
+
+                val top = boundingBox.top * scaleFactor
+                val bottom = boundingBox.bottom * scaleFactor
+                val left = boundingBox.left * scaleFactor
+                val right = boundingBox.right * scaleFactor
+
+                // Draw bounding box around detected objects
+                val drawableRect = RectF(left, top, right, bottom)
+                canvas.drawRect(drawableRect, boxPaint)
+
+                // Create text to display alongside detected objects
+                val drawableText =
+                    detection.categories()[0].categoryName() +
+                            " " +
+                            String.format(
+                                "%.2f",
+                                detection.categories()[0].score()
+                            )
+
+                // Draw rect behind display text
+                textBackgroundPaint.getTextBounds(
+                    drawableText,
+                    0,
+                    drawableText.length,
+                    bounds
+                )
+                val textWidth = bounds.width()
+                val textHeight = bounds.height()
+                canvas.drawRect(
+                    left,
+                    top,
+                    left + textWidth + Companion.BOUNDING_RECT_TEXT_PADDING,
+                    top + textHeight + Companion.BOUNDING_RECT_TEXT_PADDING,
+                    textBackgroundPaint
+                )
+
+                // Draw text for detected object
+                canvas.drawText(
+                    drawableText,
+                    left,
+                    top + bounds.height(),
+                    textPaint
+                )
+            }
+        }
+    }
+
+    fun setResults(
+        detectionResults: ObjectDetectionResult,
+        imageHeight: Int,
+        imageWidth: Int,
+    ) {
+        results = detectionResults
+
+        // Images, videos and camera live streams are displayed in FIT_START mode. So we need to scale
+        // up the bounding box to match with the size that the images/videos/live streams being
+        // displayed.
+        scaleFactor = min(width * 1f / imageWidth, height * 1f / imageHeight)
+
+        invalidate()
+    }
+
+    companion object {
+        private const val BOUNDING_RECT_TEXT_PADDING = 8
+    }
 }
