5b16eed0fcf3fdf9c924a7ad20ced00b8394b260
Paul Ruiz <ptruiz@google.com>
2022-12-02 12:16:07 -0700

    using built-in customs for hand landmark items

--- examples/hand_landmarker/android/app/src/main/java/com/google/mediapipe/examples/handlandmarker/HandLandmarkerHelper.kt
+++ examples/hand_landmarker/android/app/src/main/java/com/google/mediapipe/examples/handlandmarker/HandLandmarkerHelper.kt
@@ -365,29 +365,6 @@ class HandLandmarkerHelper(
         const val DEFAULT_NUM_HANDS = 1
         const val OTHER_ERROR = 0
         const val GPU_ERROR = 1
-
-        // Values representing the hand landmark types that the model can detect
-        const val WRIST = 0
-        const val THUMB_CMC = 1
-        const val THUMB_MCP = 2
-        const val THUMB_IP = 3
-        const val THUMB_TIP = 4
-        const val INDEX_FINGER_MCP = 5
-        const val INDEX_FINGER_PIP = 6
-        const val INDEX_FINGER_DIP = 7
-        const val INDEX_FINGER_TIP = 8
-        const val MIDDLE_FINGER_MCP = 9
-        const val MIDDLE_FINGER_PIP = 10
-        const val MIDDLE_FINGER_DIP = 11
-        const val MIDDLE_FINGER_TIP = 12
-        const val RING_FINGER_MCP = 13
-        const val RING_FINGER_PIP = 14
-        const val RING_FINGER_DIP = 15
-        const val RING_FINGER_TIP = 16
-        const val PINKY_MCP = 17
-        const val PINKY_PIP = 18
-        const val PINKY_DIP = 19
-        const val PINKY_TIP = 20
     }
 
     data class ResultBundle(
