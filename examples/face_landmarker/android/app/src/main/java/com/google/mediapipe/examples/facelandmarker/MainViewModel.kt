af263b8f24185d1197e5ed370932bb1af1544750
Paul Ruiz <ptruiz@google.com>
2023-05-01 16:15:26 -0600

    face landmarker for feature branch

---
+++ examples/face_landmarker/android/app/src/main/java/com/google/mediapipe/examples/facelandmarker/MainViewModel.kt
@@ -0,0 +1,63 @@
+package com.google.mediapipe.examples.facelandmarker
+/*
+ * Copyright 2023 The TensorFlow Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *             http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import androidx.lifecycle.ViewModel
+
+/**
+ *  This ViewModel is used to store face landmarker helper settings
+ */
+class MainViewModel : ViewModel() {
+
+    private var _delegate: Int = FaceLandmarkerHelper.DELEGATE_CPU
+    private var _minFaceDetectionConfidence: Float =
+        FaceLandmarkerHelper.DEFAULT_FACE_DETECTION_CONFIDENCE
+    private var _minFaceTrackingConfidence: Float = FaceLandmarkerHelper
+        .DEFAULT_FACE_TRACKING_CONFIDENCE
+    private var _minFacePresenceConfidence: Float = FaceLandmarkerHelper
+        .DEFAULT_FACE_PRESENCE_CONFIDENCE
+    private var _maxFaces: Int = FaceLandmarkerHelper.DEFAULT_NUM_FACES
+
+    val currentDelegate: Int get() = _delegate
+    val currentMinFaceDetectionConfidence: Float
+        get() =
+            _minFaceDetectionConfidence
+    val currentMinFaceTrackingConfidence: Float
+        get() =
+            _minFaceTrackingConfidence
+    val currentMinFacePresenceConfidence: Float
+        get() =
+            _minFacePresenceConfidence
+    val currentMaxFaces: Int get() = _maxFaces
+
+    fun setDelegate(delegate: Int) {
+        _delegate = delegate
+    }
+
+    fun setMinFaceDetectionConfidence(confidence: Float) {
+        _minFaceDetectionConfidence = confidence
+    }
+    fun setMinFaceTrackingConfidence(confidence: Float) {
+        _minFaceTrackingConfidence = confidence
+    }
+    fun setMinFacePresenceConfidence(confidence: Float) {
+        _minFacePresenceConfidence = confidence
+    }
+
+    fun setMaxFaces(maxResults: Int) {
+        _maxFaces = maxResults
+    }
+}
