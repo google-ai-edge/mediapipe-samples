2176f45964d01e5a74118e1690252c4410d9e48a
Paul Ruiz <ptruiz@google.com>
2023-05-03 08:56:47 -0600

    added language_detector sample

--- examples/language_detector/app/src/main/java/com/google/mediapipe/examples/languagedetector/MainActivity.kt
+++ examples/language_detector/app/src/main/java/com/google/mediapipe/examples/languagedetector/MainActivity.kt
@@ -1,9 +1,24 @@
+/*
+ * Copyright 2023 The TensorFlow Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *             http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.mediapipe.examples.languagedetector
 
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.mediapipe.examples.languagedetector.databinding.ActivityMainBinding
 import com.google.mediapipe.tasks.text.languagedetector.LanguageDetectorResult
