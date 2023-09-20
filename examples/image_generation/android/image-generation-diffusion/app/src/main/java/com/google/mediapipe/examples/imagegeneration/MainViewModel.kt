cb6f55049d0c4899338fa8460b99e5a07ba31baf
Paul Ruiz <ptruiz@google.com>
2023-09-20 11:26:20 -0600

    split samples into multiples for image generation, got face plugin working

---
+++ examples/image_generation/android/image-generation-diffusion/app/src/main/java/com/google/mediapipe/examples/imagegeneration/MainViewModel.kt
@@ -0,0 +1,180 @@
+package com.google.mediapipe.examples.imagegeneration
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.os.Handler
+import android.os.Looper
+import androidx.lifecycle.ViewModel
+import com.google.mediapipe.examples.imagegeneration.helper.ImageGenerationHelper
+import com.google.mediapipe.framework.image.BitmapImageBuilder
+import com.google.mediapipe.tasks.vision.imagegenerator.ImageGenerator
+import com.google.mediapipe.tasks.vision.imagegenerator.ImageGenerator.ConditionOptions.ConditionType
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(UiState())
+    private var helper: ImageGenerationHelper? = null
+    val uiState: StateFlow<UiState> = _uiState
+    private val MODEL_PATH = "/data/local/tmp/image_generator/bins/"
+
+    fun updateDisplayIteration(displayIteration: Int?) {
+        _uiState.update { it.copy(displayIteration = displayIteration) }
+    }
+
+    fun updateDisplayOptions(displayOptions: DisplayOptions) {
+        _uiState.update { it.copy(displayOptions = displayOptions) }
+    }
+
+    fun updatePrompt(prompt: String) {
+        _uiState.update { it.copy(prompt = prompt) }
+    }
+
+    fun updateIteration(iteration: Int?) {
+        _uiState.update { it.copy(iteration = iteration) }
+    }
+
+    fun updateSeed(seed: Int?) {
+        _uiState.update { it.copy(seed = seed) }
+    }
+
+    fun clearError() {
+        _uiState.update { it.copy(error = null) }
+    }
+
+    fun initializeImageGenerator() {
+        val displayIteration = _uiState.value.displayIteration
+        val displayOptions = _uiState.value.displayOptions
+        try {
+            if (displayIteration == null && displayOptions == DisplayOptions.ITERATION) {
+                _uiState.update { it.copy(error = "Display iteration cannot be empty") }
+                return
+            }
+
+            _uiState.update { it.copy(isInitializing = true) }
+            val mainLooper = Looper.getMainLooper()
+            GlobalScope.launch {
+                helper?.initializeImageGenerator(MODEL_PATH)
+                Handler(mainLooper).post {
+                    _uiState.update {
+                        it.copy(
+                            initialized = true, isInitializing = false
+                        )
+                    }
+                }
+            }
+
+        } catch (e: Exception) {
+            _uiState.update {
+                it.copy(
+                    error = e.message
+                        ?: "Error initializing image generation model",
+                )
+            }
+        }
+    }
+
+    // Create image generation helper
+    fun createImageGenerationHelper(context: Context) {
+        helper = ImageGenerationHelper(context)
+    }
+
+    fun generateImage() {
+        val prompt = _uiState.value.prompt
+        val iteration = _uiState.value.iteration
+        val seed = _uiState.value.seed
+        if (prompt.isEmpty()) {
+            _uiState.update { it.copy(error = "Prompt cannot be empty") }
+            return
+        }
+        if (iteration == null) {
+            _uiState.update { it.copy(error = "Iteration cannot be empty") }
+            return
+        }
+        if (seed == null) {
+            _uiState.update { it.copy(error = "Seed cannot be empty") }
+            return
+        }
+        _uiState.update { it.copy(isGenerating = true) }
+
+
+        // Generate without iterations
+//        val mainLooper = Looper.getMainLooper()
+//        GlobalScope.launch {
+//            val image = helper?.generate()
+//            Handler(mainLooper).post {
+//                _uiState.update {
+//                    it.copy(
+//                        isGenerating = false,
+//                        outputBitmap = image
+//                    )
+//                }
+//            }
+
+
+        // Generate with iterations
+        val mainLooper = Looper.getMainLooper()
+        val tmpIteration = 10
+        _uiState.update { it.copy(displayIteration = 1) }
+        GlobalScope.launch {
+
+            // if display option is final, use generate method, else use execute method
+            if (uiState.value.displayOptions == DisplayOptions.FINAL) {
+                val result = helper?.generate()
+                _uiState.update {
+                    it.copy(outputBitmap = result)
+                }
+            } else {
+                helper?.setInput(prompt, tmpIteration, seed)
+
+                val displayIteration = _uiState.value.displayIteration ?: 0
+                for (step in 0 until tmpIteration) {
+
+                    val result =
+                        helper?.execute((displayIteration > 0 && ((step + 1) % displayIteration == 0)))
+
+                    _uiState.update {
+                        it.copy(
+                            generatingMessage = "Generating...", outputBitmap = result
+                        )
+                    }
+                }
+            }
+            _uiState.update {
+                it.copy(
+                    isGenerating = false, generatingMessage = "Generate"
+                )
+            }
+//
+        }
+    }
+
+    fun resetUiState() {
+        _uiState.update {
+            UiState()
+        }
+        helper = null
+    }
+}
+
+data class UiState(
+    val error: String? = null,
+    val outputBitmap: Bitmap? = null,
+    val displayOptions: DisplayOptions = DisplayOptions.FINAL,
+    val displayIteration: Int? = null,
+    val prompt: String = "",
+    val iteration: Int? = null,
+    val seed: Int? = null,
+    val initialized: Boolean = false,
+    val initializedDisplayIteration: Int? = null,
+    val isGenerating: Boolean = false,
+    val isInitializing: Boolean = false,
+    val generatingMessage: String = "",
+)
+
+enum class DisplayOptions {
+    ITERATION, FINAL
+}
---
+++ examples/image_generation/android/image-generation-plugins/app/src/main/java/com/google/mediapipe/imagegeneration/imagegeneration/plugins/helper/ImageGenerationHelper.kt
@@ -0,0 +1,136 @@
+package com.google.mediapipe.examples.imagegeneration.plugins.helper
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import com.google.mediapipe.framework.image.BitmapExtractor
+import com.google.mediapipe.framework.image.MPImage
+import com.google.mediapipe.tasks.core.BaseOptions
+import com.google.mediapipe.tasks.vision.imagegenerator.ImageGenerator
+import com.google.mediapipe.tasks.vision.imagegenerator.ImageGenerator.ConditionOptions
+import com.google.mediapipe.tasks.vision.imagegenerator.ImageGenerator.ConditionOptions.ConditionType
+import com.google.mediapipe.tasks.vision.imagegenerator.ImageGenerator.ConditionOptions.EdgeConditionOptions
+import com.google.mediapipe.tasks.vision.imagegenerator.ImageGenerator.ConditionOptions.FaceConditionOptions
+import com.google.mediapipe.tasks.vision.imagegenerator.ImageGenerator.ImageGeneratorOptions
+
+
+class ImageGenerationHelper(
+    val context: Context
+) {
+
+    lateinit var imageGenerator: ImageGenerator
+
+    fun initializeFaceImageGenerator(modelPath: String) {
+        val options = ImageGeneratorOptions.builder().setImageGeneratorModelDirectory(modelPath)
+            .build()
+
+        val faceModelBaseOptions = BaseOptions.builder()
+            .setModelAssetPath("face_landmarker.task")
+            .build()
+
+        val pluginModelBaseOptions = BaseOptions.builder()
+            .setModelAssetPath("face_landmark_plugin.tflite")
+            .build()
+
+        val faceConditionOptions = FaceConditionOptions.builder()
+            .setFaceModelBaseOptions(faceModelBaseOptions)
+            .setPluginModelBaseOptions(pluginModelBaseOptions)
+            .setMinFaceDetectionConfidence(0.3f)
+            .setMinFacePresenceConfidence(0.3f)
+            .build()
+
+        val conditionOptions = ConditionOptions.builder().setFaceConditionOptions(faceConditionOptions).build()
+        imageGenerator = ImageGenerator.createFromOptions(context, options, conditionOptions)
+    }
+
+    fun initializeEdgeImageGenerator(modelPath: String) {
+        /*
+        val options = ImageGeneratorOptions.builder().setImageGeneratorModelDirectory(modelPath)
+            .build()
+
+        val edgeModelBaseOptions = BaseOptions.builder()
+            .setModelAssetPath(...)
+            .build()
+
+        val pluginModelBaseOptions = BaseOptions.builder()
+//            .setModelAssetPath(modelPath)
+            .build()
+
+        val edgeConditionOptions = EdgeConditionOptions.builder()
+            .setThreshold1(100.0f) // default = 100.0f
+            .setThreshold2(100.0f) // default = 100.0f
+            .setApertureSize(3) // default = 3
+            .setL2Gradient(false) // default = false
+            .setPluginModelBaseOptions(pluginModelBaseOptions)
+            .build()
+
+        val conditionOptions = ConditionOptions.builder().setEdgeConditionOptions(edgeConditionOptions).build()
+        imageGenerator = ImageGenerator.createFromOptions(context, options, conditionOptions)
+
+         */
+    }
+
+    fun initializeDepthImageGenerator(modelPath: String) {
+        /*
+        val options = ImageGeneratorOptions.builder().setImageGeneratorModelDirectory(modelPath)
+            .build()
+
+        val depthModelBaseOptions = BaseOptions.builder()
+            .setModelAssetPath(...)
+            .build()
+
+        val pluginModelBaseOptions = BaseOptions.builder()
+//            .setModelAssetPath(modelPath)
+            .build()
+
+        val depthConditionOptions = ConditionOptions.DepthConditionOptions.builder()
+            .setDepthModelBaseOptions(depthModelBaseOptions)
+            .setPluginModelBaseOptions(pluginModelBaseOptions)
+            .build()
+
+        val conditionOptions = ConditionOptions.builder().setDepthConditionOptions(depthConditionOptions).build()
+        imageGenerator = ImageGenerator.createFromOptions(context, options, conditionOptions)
+
+         */
+    }
+
+    fun setInput(prompt: String, conditionalImage: MPImage, conditionType: ConditionType, iteration: Int, seed: Int) {
+        imageGenerator.setInputs(prompt, conditionalImage, conditionType, iteration, seed)
+    }
+
+    fun generate(): Bitmap {
+        // TODO Needs update for fix
+        val result = imageGenerator.generate("purple teapot sitting on a green table", 10, 0)
+        val bitmap = BitmapExtractor.extract(result?.generatedImage())
+        return bitmap
+    }
+
+    fun execute(showResult: Boolean): Bitmap {
+        // execute image generation model
+        val result = imageGenerator.execute(showResult)
+
+//        val image = result?.generatedImage()
+        if( result == null || result.generatedImage() == null ) {
+            return Bitmap.createBitmap(512, 512, Bitmap.Config.ARGB_8888).apply {
+                val canvas = Canvas(this)
+                val paint = Paint()
+                paint.color = Color.WHITE
+                canvas.drawPaint(paint)
+            }
+        }
+
+        val bitmap =
+            BitmapExtractor.extract(result?.generatedImage())
+
+
+//        val width = image!!.width
+//        val height = image.height
+//
+//        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+//        bitmap.copyPixelsFromBuffer(byteBuffer)
+
+        return bitmap
+    }
+}
