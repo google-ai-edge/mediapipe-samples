1132ef022cf40cf6d99c44b24d04037a6c8f310c
Achim Klenk <s0577479@student.htw-berlin.de>
2023-10-15 11:29:31 +0200

    Change camera lens to facing_back

--- examples/gesture_recognizer/android/app/src/main/java/com/google/mediapipe/examples/gesturerecognizer/fragment/CameraFragment.kt
+++ examples/gesture_recognizer/android/app/src/main/java/com/google/mediapipe/examples/gesturerecognizer/fragment/CameraFragment.kt
@@ -65,7 +65,7 @@ class CameraFragment : Fragment(),
     private var imageAnalyzer: ImageAnalysis? = null
     private var camera: Camera? = null
     private var cameraProvider: ProcessCameraProvider? = null
-    private var cameraFacing = CameraSelector.LENS_FACING_FRONT
+    private var cameraFacing = CameraSelector.LENS_FACING_BACK
 
     /** Blocking ML operations are performed using this executor */
     private lateinit var backgroundExecutor: ExecutorService
