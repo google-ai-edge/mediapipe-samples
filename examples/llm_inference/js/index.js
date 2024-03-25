252a41d156ff4f10d4ae7a410abb2277505abd2e
Linchenn <40653845+Linchenn@users.noreply.github.com>
2024-03-25 10:19:44 -0700

    Add parameters for LLM sampling (#324)
    * Add parameters for sampling
    
    * Update index.js
    
    * Update index.js

--- examples/llm_inference/js/index.js
+++ examples/llm_inference/js/index.js
@@ -52,12 +52,25 @@ async function runDemo() {
 
   submit.value = 'Loading the model...'
   LlmInference
-      .createFromModelPath(genaiFileset, modelFileName)
+      .createFromOptions(genaiFileset, {
+        baseOptions: {modelAssetPath: modelFileName},
+        // maxTokens: 512,  // The maximum number of tokens (input tokens + output
+        //                  // tokens) the model handles.
+        // randomSeed: 1,   // The random seed used during text generation.
+        // topK: 1,  // The number of tokens the model considers at each step of
+        //           // generation. Limits predictions to the top k most-probable
+        //           // tokens. Setting randomSeed is required for this to make
+        //           // effects.
+        // temperature:
+        //     1.0,  // The amount of randomness introduced during generation.
+        //           // Setting randomSeed is required for this to make effects.
+      })
       .then(llm => {
         llmInference = llm;
         submit.disabled = false;
         submit.value = 'Get Response'
-      }).catch(() =>{
+      })
+      .catch(() => {
         alert('Failed to initialize the task.');
       });
 }
