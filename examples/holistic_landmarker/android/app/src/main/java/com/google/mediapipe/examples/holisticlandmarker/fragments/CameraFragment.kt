35734bd6bd721da13200668bb698fadd40033ec8
st-vinhthai <vinh.thai@supremetech.vn>
2024-03-26 16:21:29 +0700

    feat: add recyclerviewResults visible condition

--- examples/holistic_landmarker/android/app/src/main/java/com/google/mediapipe/examples/holisticlandmarker/fragments/CameraFragment.kt
+++ examples/holistic_landmarker/android/app/src/main/java/com/google/mediapipe/examples/holisticlandmarker/fragments/CameraFragment.kt
@@ -32,6 +32,7 @@ import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.Navigation
@@ -143,6 +144,8 @@ class CameraFragment : Fragment(),
         }
         setUpListener()
         viewModel.helperState.observe(viewLifecycleOwner) { helperState ->
+            // Only show face blend results view when Face Blend Mode available
+            fragmentCameraBinding.recyclerviewResults.isVisible = helperState.isFaceBlendMode
             updateBottomSheetControlsUi(helperState)
         }
     }
