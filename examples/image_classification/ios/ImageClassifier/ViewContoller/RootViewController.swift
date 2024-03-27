2b2f6e6fe6b81e76a0a6db15917f649458d46dc6
tuanmaia <tuan.mai@supremetech.vn>
2024-03-27 15:20:06 +0700

    update button top space

--- examples/image_classification/ios/ImageClassifier/ViewContoller/RootViewController.swift
+++ examples/image_classification/ios/ImageClassifier/ViewContoller/RootViewController.swift
@@ -38,7 +38,7 @@ class RootViewController: UIViewController {
   private struct Constants {
     static let inferenceBottomHeight = 284.0
     static let expandButtonHeight = 41.0
-    static let expandButtonTopSpace = 10.0
+    static let expandButtonTopSpace = 20.0
     static let mediaLibraryViewControllerStoryBoardId = "MEDIA_LIBRARY_VIEW_CONTROLLER"
     static let cameraViewControllerStoryBoardId = "CAMERA_VIEW_CONTROLLER"
     static let storyBoardName = "Main"
