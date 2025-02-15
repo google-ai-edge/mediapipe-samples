0aecdbbecca071260f9cf3d7ccd55ae71f7f1fb1
Jun Jiang <junjiang@google.com>
2025-02-15 11:16:28 -0800

    Make the login page background adapt to system theme.

--- examples/llm_inference/android/app/build.gradle.kts
+++ examples/llm_inference/android/app/build.gradle.kts
@@ -80,6 +80,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("com.google.android.material:material:1.12.0")
 
     implementation ("com.google.mediapipe:tasks-genai:0.10.21")
 
