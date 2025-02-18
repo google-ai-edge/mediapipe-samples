f979b4b1b980bd7b5b1ff3cf45ac4e8d809fab58
Jun Jiang <junjiang@google.com>
2025-02-17 21:57:03 -0800

    Use Light/Dark color scheme instead of dynamic color scheme.

--- examples/llm_inference/android/app/src/main/java/com/google/mediapipe/examples/llminference/ui/theme/Theme.kt
+++ examples/llm_inference/android/app/src/main/java/com/google/mediapipe/examples/llminference/ui/theme/Theme.kt
@@ -35,11 +35,6 @@ fun LLMInferenceTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
